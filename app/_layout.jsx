2782ef210a3cb3609cffa8efc4546f782cd0f5e0
Gauti-tag <gautier.tiehoule@gmail.com>
2024-10-08 13:45:27 +0000

    Feat: Add the login route

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -10,6 +10,11 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" />
+      <Stack.Screen name="login/index"
+        options={{
+            headerShown: false /** Add this option to hide the header */
+        }}
+      />
     </Stack>
   );
 }
